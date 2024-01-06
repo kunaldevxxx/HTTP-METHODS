565eea9e858d47430d7cc395cb292b1bf8f8c6bb
Kunal Khare <136145336+kunaldevxxx@users.noreply.github.com>
2024-01-06 23:40:35 +0530

    JUST BASIC URL HANDLING || URL PACKAGE USE

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const http=require('http');
-const fs = require('fs')
+const fs = require('fs');
+const url = require('url');
 const myserver=http.createServer((req,res)=>{
+    if(req.url==="/favicon.ico") return res.end();
     const log=`${Date.now}:${req.url}\n`
+    const myUrl=url.parse(req.url,true);
+    console.log(myUrl)
     fs.appendFile('log.txt',log,(err,data)=>{
-       switch(req.url){
+       switch(myUrl.pathname){
         case '/':res.end("Home");
         break
-        case '/about':res.end("I Am Kunal");
+        case '/about':
+            const username=myUrl.query.myname
+            res.end(`Hi,${username}`);
         break 
         default:
             res.end("404 Not Found");
        }
     })
 console.log("New Request Recieved");
-console.log(req.headers)
+
 console.log("Hello From Server");
 });
 
